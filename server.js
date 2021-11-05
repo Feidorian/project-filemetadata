ea0e1492890d6bea66270a215968e17ba769ce8b
Brian O'Mile <brianmile10@yahoo.com>
2021-11-05 05:28:45 +0000

    challenge completed

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
-
+const multer = require('multer');
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null,__dirname+'/uploads')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  }
+});
+var upload = multer({ storage: storage });
+app.post('/api/fileanalyse', upload.single('upfile'), (req,res) => {
+  const {filename:name,mimetype:type, size} = req.file;
+  res.json({name,type,size})
+})
 
 app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
